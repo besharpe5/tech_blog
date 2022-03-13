29d1fda72baef846562ca2a7dd52ce09ed8d35bd
besharpe5 <besharpe15@gmail.com>
2022-03-12 22:10:54 -0500

    added User model

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const Comment = require('./Comment');
+const Post = require('./Post');
+const User = require('./User');
+
+// associations are created in the code below
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
