563f59c18b1a962077feac3de4e63390fc642aea
besharpe5 <besharpe15@gmail.com>
2022-03-12 21:37:42 -0500

    added content to connection.js, .env, and server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// this will start routes
+app.use(routes);
+
+// this will start the connection to the db and the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on the local host'));
+});
